acb0f39913c9796fd82abfcc4d715017d9e77463
Maor <Maor@MAOR-PC>
2022-03-05 19:49:58 +0200

    Add "to private profile" button

--- app/src/main/java/com/finalProject/travelTogether/feed/PostListRvViewModel.java
+++ app/src/main/java/com/finalProject/travelTogether/feed/PostListRvViewModel.java
@@ -2,12 +2,9 @@ package com.finalProject.travelTogether.feed;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
-
 import com.finalProject.travelTogether.feed.relations.PostAndUser;
 import com.finalProject.travelTogether.model.Model;
-import com.finalProject.travelTogether.model.Post;
 import com.finalProject.travelTogether.model.User;
-import com.google.firebase.auth.FirebaseAuth;
 import java.util.List;
 
 public class PostListRvViewModel extends ViewModel {
